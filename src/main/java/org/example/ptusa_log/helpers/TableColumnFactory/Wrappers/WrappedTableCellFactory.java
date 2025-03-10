97926cfc15d7bc9f30dfcfc88c83c0d6f56403de
kirathenics <bka32129@gmail.com>
2025-03-10 11:48:47 +0300

    fixed table cell text color

--- src/main/java/org/example/ptusa_log/helpers/TableColumnFactory/Wrappers/WrappedTableCellFactory.java
+++ src/main/java/org/example/ptusa_log/helpers/TableColumnFactory/Wrappers/WrappedTableCellFactory.java
@@ -1,12 +1,15 @@
 package org.example.ptusa_log.helpers.TableColumnFactory.Wrappers;
 
+import javafx.beans.binding.Bindings;
 import javafx.scene.control.TableCell;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class WrappedTableCellFactory {
     public static <T> TableCell<T, String> createWrappedCell() {
         return new TableCell<>() {
             private final Text text = new Text();
+
             {
                 text.wrappingWidthProperty().bind(widthProperty());
             }
@@ -19,6 +22,11 @@ public class WrappedTableCellFactory {
                 } else {
                     text.setText(item);
                     setGraphic(text);
+
+                    text.fillProperty().bind(Bindings.when(tableRowProperty().isNotNull()
+                                    .and(tableRowProperty().get().selectedProperty()))
+                            .then(Color.WHITE)
+                            .otherwise(Color.BLACK));
                 }
             }
         };
